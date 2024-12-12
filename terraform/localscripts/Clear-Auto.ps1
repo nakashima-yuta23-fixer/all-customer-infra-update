8ee99574d666e8611ef920c6a0a52906ffb992eb
nakashima-yuta23-fixer <nakashima-yuta23@fixer.cd.jp>
2024-12-12 11:29:25 +0900

    fix

--- terraform/localscripts/Clear-Auto.ps1
+++ terraform/localscripts/Clear-Auto.ps1
@@ -10,5 +10,4 @@ echo $directory
 
 Get-ChildItem -Path $directory -Recurse -Filter "provider.tf" | Remove-Item -Force
 Get-ChildItem -Path $directory -Recurse -Filter "backend.tf" | Remove-Item -Force
-Get-ChildItem -Path $directory -Recurse -Filter ".terraform.lock.hcl" | Remove-Item -Force
-Get-ChildItem -Path $directory -Recurse -Directory -Filter ".terraform" | Remove-Item -Recurse -Force
+Get-ChildItem -Path $directory -Recurse -Filter ".terraform.lock.hcl" | Remove-Item -ForceGet-ChildItem -Path $directory -Recurse -Directory -Filter ".terraform" | Remove-Item -Recurse -Force
